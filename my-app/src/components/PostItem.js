e9d34541bc0913fcfef7eece2c47a9e0ad4be62d
JustinFourie1993 <>
2023-11-05 13:29:37 +0000

    add searchbar to content.js

--- my-app/src/components/PostItem.js
+++ my-app/src/components/PostItem.js
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from "react"
 import css from "./css/PostItem.module.css";
 
-function PostItem(props) {
-    
-  return (
 
-        props.posts.map(post => {
-                    return (
-                    <div key={post.title} className={css.SearchItem}>
-                        <p>Title: {post.title}</p>
-                        <p>Artist: {post.name}</p>
-                        <img src={post.image}></img>
-                        <p>Description: {post.description}</p>
-                    </div>
-                    )
-                })
-    
-  )
+function PostItem(props) {
+    return (
+        props.savedPosts.map(post => {
+            // Extra task destructuring 
+            const {title, name, image, description} = post
+            return <div className={css.SearchItem} key={title}>
+                <p>{title}</p>
+                <p>{name}</p>
+                <img src={image} alt="random"/>
+                <p>{description}</p>
+            </div>
+            }
+        )
+    )
 }
 
 export default PostItem
